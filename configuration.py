017d351e8bdf2db17f2ebb011e39dc149dbf03b6
terranigmark <linnk99@gmail.com>
2025-03-06 19:23:02 -0600

    Definidas constantes para peticiones y creada funcion base para traer todos los kits

---
+++ configuration.py
@@ -0,0 +1,2 @@
+BASE_URL = 'https://cnt-9c56ec38-0653-437d-b24d-4d4ee2f26256.containerhub.tripleten-services.com'
+KITS = '/api/v1/kits'
---
+++ urban_grocers_methods.py
@@ -0,0 +1,6 @@
+from configuration import BASE_URL, KITS
+import requests
+
+
+def get_all_kits():
+    pass
