12ca3bddbbba77598f0476f592879dadd578538a
terranigmark <linnk99@gmail.com>
2025-03-06 19:33:03 -0600

    Funcion para traer los kits definida con parametro cardID

--- urban_grocers_methods.py
+++ urban_grocers_methods.py
@@ -1,6 +1,8 @@
+import data
 from configuration import BASE_URL, KITS
 import requests
 
 
 def get_all_kits():
-    pass
+    response = requests.get(BASE_URL + KITS + data.card_id_1)
+    return response
